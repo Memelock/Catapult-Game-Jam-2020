150b7dbe9badce3cea246c08ba612a9d4ceba974
Memelock <universalcoder8@gmail.com>
2020-10-17 13:13:45 -0400

    Merge A* FOW
    Merge of the A* and the FogOfWar so there are two scenes in the project

---
+++ Catapult Project/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    Transform player;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, -10f);
+    }
+}
