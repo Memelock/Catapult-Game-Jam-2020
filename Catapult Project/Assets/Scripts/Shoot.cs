4976e2ac2a14c20085614278c76fac65ac0abc1b
Ruel <ruey1@comcast.net>
2020-10-17 14:18:21 -0400

    Scripts for Boulder and Shoot.
    Attach Boulder.cs to bulletPrefab. Attach Shoot.cs to Player object.

---
+++ Catapult Project/Assets/Scripts/Shoot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    /*
+     * BulletPrefab: add boulder to shoot.
+     * BulletSpeed: How Fast bullet arrives at destination.
+     * Endpoint: Game object that the bullet is launched towards. Invisible
+     */
+    public GameObject BulletPrefab;
+    public float BulletSpeed = 25.0f;
+    public GameObject EndPoint;
+    
+    private bool shotFired;
+
+    public bool ShotFired
+    {
+        set => shotFired = value;
+    }   
+ 
+
+    private void Fire()
+    {
+        Vector2 target = GameObject.FindWithTag("EndPoint").GetComponent<Transform>().transform.position - gameObject.transform.position;
+        float targetMidPoint = target.y / 2.0f;
+        GameObject bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(target * BulletSpeed);
+        
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && shotFired == false)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 0;
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            ShotFired = true;
+            Instantiate(EndPoint, worldPos, Quaternion.identity);
+
+            Fire();
+        }
+    }
+
+}
