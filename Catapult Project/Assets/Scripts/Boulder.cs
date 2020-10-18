b6de901842eead48b43173cafb04f18d87c91154
DavidKozdra <45106564+DavidKozdra@users.noreply.github.com>
2020-10-18 13:29:33 -0400

    :(
    Fix this andreww

--- Catapult Project/Assets/Scripts/Boulder.cs
+++ Catapult Project/Assets/Scripts/Boulder.cs
@@ -7,21 +7,36 @@ public class Boulder : MonoBehaviour
 {
     public GameObject FOW;
     public GameObject Player;
-    bool haslanded;
+    public bool haslanded = false;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        
+
         if (collider.tag == "EndPoint")
         {
             GameObject.Instantiate(FOW, gameObject.transform.position, Quaternion.identity);
-                Destroy(collider.gameObject);
-                Destroy(gameObject.GetComponent<CircleCollider2D>());
-                Destroy(gameObject.GetComponent<Rigidbody2D>());
-                GameObject.Find("Catapult").GetComponent<Shoot>().ShotFired = false;
-            
+            Destroy(collider.gameObject);
+            //Destroy(gameObject.GetComponent<Rigidbody2D>());
+            rb.velocity = new Vector2(0, 0);
+            GameObject.Find("Catapult").GetComponent<Shoot>().ShotFired = false;
+            haslanded = true;
+
+            StartCoroutine(ExecuteAfterTime(.2f));
         }
     }
 
-    
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        haslanded = false;
+        Destroy(gameObject.GetComponent<CircleCollider2D>());
+    }
+
 }
